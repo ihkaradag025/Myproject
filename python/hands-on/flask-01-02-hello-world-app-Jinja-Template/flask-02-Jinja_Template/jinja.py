3c3759b017fbd0837f5f4843768a6a263cfcde6d
ihkaradag025 <81003301+ihkaradag025@users.noreply.github.com>
2021-06-15 22:51:18 +0300

    Update jinja.py

--- python/hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-02-Jinja_Template/jinja.py
+++ python/hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-02-Jinja_Template/jinja.py
@@ -9,7 +9,7 @@ def head():
 @app.route('mult')
 def number():
     var1, var2 =23,45
-    return render_template('body.html',num1=var1,num2=var2,num1*num2 )
+    return render_template('body.html',num1=var1,num2=var2,x=num1*num2 )
 
 
 
